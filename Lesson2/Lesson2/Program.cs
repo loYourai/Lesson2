538ba586256df8b4dfc7573679c2d0dc13b18465
Danylo Raduchych <danil.r87602@gmail.com>
2023-04-29 10:45:25 +0200

    Add operators examples

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,161 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Runtime.ExceptionServices;
+using System.Xml;
+/*
+int first = 10;
+int second = 5;
+int third = first + second;
+*/
+
+
+
+
+
+/*
+Console.WriteLine(10/2);
+Console.WriteLine(11/2);
+Console.WriteLine(12/2);
+Console.WriteLine(13/2);
+*/
+/*
+double a = 6.2;
+float b = 6.2F;         операции над дробными типами
+decimal d = 6.2M;
+Console.WriteLine(a/3);
+Console.WriteLine(b/3);
+Console.WriteLine(a/3);
+*/
+
+
+
+// мультипликации
+/*
+int a = 10;
+int b = 5;
+Console.WriteLine(a * b);
+*/
+
+
+
+
+//int first = 1;
+//Console.WriteLine(++first);
+//Console.WriteLine(first);
+
+
+
+
+/* остатток от деления
+int a = 13;
+int b = 5;
+Console.WriteLine(a / b);
+Console.WriteLine(a % b );
+*/
+
+//comparison сравнение
+/*
+int first = 12;
+int second = 12;
+bool result = first >= second;
+bool secondResult = first < second;
+
+
+Console.WriteLine(first != second);
+*/
+//Console.WriteLine(result == secondResult); //true == false ==> false
+/*
+int a = 10;
+int b = int.MaxValue;
+long l =long.MaxValue;
+
+// unsigned
+ulong r =ulong.MaxValue;
+short s = short.MaxValue;
+short s1=short.MinValue;
+
+
+Console.WriteLine(b);
+Console.WriteLine(l);
+Console.WriteLine(r);
+Console.WriteLine(s);
+Console.WriteLine(s1);
+*/
+
+//int a = 1;
+//int b = Math.Max(1, 2);
+/*
+double a = 1.5;
+
+Console.WriteLine("Round:" + Math.Round(a));
+Console.WriteLine("Truncate:" + Math.Truncate(a));
+Console.WriteLine("Floor:" + Math.Floor(a));
+Console.WriteLine("Ceiling" + Math.Ceiling(a));
+*/
+//Console.WriteLine(b);
+
+//double a = 5.8;
+//Console.WriteLine(Math.Pow(a, 2)); // 5 * 5
+//double a = 125; //5 * 5 * 5
+//Console.WriteLine(Math.Cbrt(a)); //5
+
+
+
+/* оператосы со строками
+string a = "Hello";
+string b = " World";
+string c = a + b;
+a += b;
+
+Console.WriteLine(c);
+Console.WriteLine(a);
+*/
+//операторы с типами boll
+//bool a = false;
+//bool b = false;
+//bool c = true;
+
+
+//Console.WriteLine(a && b && c);
+/*
+bool cash = false;
+bool card = false;
+bool paypal = true;
+Console.WriteLine(cash || card || paypal);
+*/
+
+//bool a = true;
+
+//Console.WriteLine(!a);
+
+//int a = 10;
+//int b = 10;
+//Console.WriteLine(a != b);
+
+
+//intm long - numbers
+// string -text/string
+//DateTime
+/*
+DateTime date = DateTime.Now;
+int minute = date.Minute;
+int day = date.Day;
+int year = date.Year;
+Console.WriteLine("Minutes:" + minute );
+Console.WriteLine("Days:" + day);
+Console.WriteLine("Year:" + year );
+*/
+DateTime januaryFirst = new DateTime(2023, 1, 1);
+DateTime todayFirst =  DateTime.Now.Date;
+Console.WriteLine(januaryFirst < todayFirst);
+
+
+
+
+
+
+string a = Console.ReadLine();
+
+int x = Convert.ToInt32(a);
+
+Console.WriteLine( -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
