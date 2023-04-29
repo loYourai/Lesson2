d3cf0a72d4a4ba533c543142ceebeb3e4461af48
Danylo Raduchych <danil.r87602@gmail.com>
2023-04-29 11:53:05 +0200

    HOMEWORK

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data.Common;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
 using System.Xml;
 /*
 int first = 10;
@@ -145,17 +146,80 @@ Console.WriteLine("Minutes:" + minute );
 Console.WriteLine("Days:" + day);
 Console.WriteLine("Year:" + year );
 */
+/*
 DateTime januaryFirst = new DateTime(2023, 1, 1);
 DateTime todayFirst =  DateTime.Now.Date;
 Console.WriteLine(januaryFirst < todayFirst);
+*/
 
 
 
 
-
-
+/* -6*x^3+5*x^2-10*x+15
+  
+ 
 string a = Console.ReadLine();
 
 int x = Convert.ToInt32(a);
 
 Console.WriteLine( -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+
+
+string a = Console.ReadLine();
+
+long x = Convert.ToInt32(a);
+Console.WriteLine(-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+*/
+
+
+
+
+/*
+      2*pi*x 
+
+string a = Console.ReadLine();
+
+long x = Convert.ToInt32(a);
+double d = Math.PI;
+
+Console.WriteLine(2 * d * x);
+*/
+
+
+
+//    max(x, y)
+
+/*
+string a = Console.ReadLine();
+
+long x = Convert.ToInt32(a);
+
+x = long.MaxValue;
+Console.WriteLine(x);
+
+string b = Console.ReadLine();
+
+int y = Convert.ToInt32(b);
+
+y = int.MaxValue;
+Console.WriteLine(y);
+*/
+
+
+
+
+
+   // до/после нового гота
+/*
+
+Console.WriteLine("До Нового года осталось:");
+var today = DateTime.Today;
+var newYear = new DateTime(today.Year + 0, 12, 31);
+var days = (newYear - today).TotalDays;
+Console.WriteLine("{0} дней.", days);
+*/
+Console.WriteLine("после нового года пошло:");
+var today = DateTime.Today;
+var oldYear = new DateTime(today.Year - 0, 1, 1);
+var days = (today - oldYear).TotalDays;
+Console.WriteLine("{0} дней.", days);
---
+++ Lesson2/Lesson2/homework.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+   
+
+        /* -6*x^3+5*x^2-10*x+15
+  
+ 
+string a = Console.ReadLine();
+
+int x = Convert.ToInt32(a);
+
+Console.WriteLine( -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+
+
+string a = Console.ReadLine();
+
+long x = Convert.ToInt32(a);
+Console.WriteLine(-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+*/
+
+
+
+
+        /*
+              2*pi*x 
+
+        string a = Console.ReadLine();
+
+        long x = Convert.ToInt32(a);
+        double d = Math.PI;
+
+        Console.WriteLine(2 * d * x);
+        */
+
+
+
+        //    max(x, y)
+
+        /*
+        string a = Console.ReadLine();
+
+        long x = Convert.ToInt32(a);
+
+        x = long.MaxValue;
+        Console.WriteLine(x);
+
+        string b = Console.ReadLine();
+
+        int y = Convert.ToInt32(b);
+
+        y = int.MaxValue;
+        Console.WriteLine(y);
+        */
+
+
+
+
+
+        // до/после нового гота
+        /*
+
+        Console.WriteLine("До Нового года осталось:");
+        var today = DateTime.Today;
+        var newYear = new DateTime(today.Year + 0, 12, 31);
+        var days = (newYear - today).TotalDays;
+        Console.WriteLine("{0} дней.", days);
+        */
+        Console.WriteLine("после нового года пошло:");
+        var today = DateTime.Today;
+        var oldYear = new DateTime(today.Year - 0, 1, 1);
+        var days = (today - oldYear).TotalDays;
+        Console.WriteLine("{0} дней.", days);
+    
+
